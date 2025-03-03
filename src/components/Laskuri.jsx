73a37952703b7c0ae9b92b1106a684314250e3a7
Sasu Paakkunainen <42316981+sasupa@users.noreply.github.com>
2025-03-04 00:35:14 +0200

    Kuvan kanssa deploy

--- src/components/Laskuri.jsx
+++ src/components/Laskuri.jsx
@@ -1,7 +1,10 @@
+import Karhu from '..assets/karhu.jpg'
+
 const laskuri = () => {
   return (
     <div>
       <p>Laskuri tulee tähän + GitHub deploy</p>
+      <img src={Karhu} alt="verokarhu" width="300" />
     </div>
   )
 }
